2268a10e18d4576ea854e622361aeb44161f8908
rmatej22 <rmatej545@gmail.com>
2023-11-17 17:10:05 +0100

    feature: create login endpoint with LoginDto in AccountController

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,6 +37,31 @@ public class AccountController : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.UserName);
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid username.");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid password.");
+            }
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
