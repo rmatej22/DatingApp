118cb56c602ec5c94c5569849202173f1b3e0a55
rmatej22 <rmatej545@gmail.com>
2023-11-21 13:27:04 +0100

    update frontend with login and register functionality

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NavComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     CommonModule,
     BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
