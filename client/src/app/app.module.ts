6925a5c5db634c597dd24acfa82dc671254ebfbf
rmatej22 <rmatej545@gmail.com>
2023-12-22 15:34:37 +0100

    create and implement logic with not-found and server-error components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { ListsComponent } from './lists/lists.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     MemberDetailComponent,
     ListsComponent,
     TestErrorsComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     BrowserModule,
