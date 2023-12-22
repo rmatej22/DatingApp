687f2048d6b6e92f245d00a3fa3ff192d960e73a
rmatej22 <rmatej545@gmail.com>
2023-12-22 15:35:16 +0100

    remove log

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -48,7 +48,6 @@ export class ErrorInterceptor implements HttpInterceptor {
               break;
             default:
               this.toastr.error('Something unexpected went wrong');
-              console.log(error);
               break;
           }
         }
